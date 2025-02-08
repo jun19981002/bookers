1ca9215136a723016b743f75c26d85ccccf93722
EC2 Default User <193612538+jun19981002@users.noreply.github.com>
2025-02-08 12:39:01 +0000

    [Finish] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,16 +27,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-      book.update(book_params)
-      flash[:notice] = "Book was successfully created."
-    if book.save
-      redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
     else
-      @book = Book.find(params[:id])
       render :edit
     end
-
   end
   
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   get 'books/new'
   get 'books/edit'
   get 'top' => 'homes#top'
